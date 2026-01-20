04432fca9d41a72cc28adb5421d17311614c800c
Robert Jansen <me@rjns.dev>
2026-01-20 23:38:48 +0100

    typo

--- application/src/routes/api/servers/_server_/ws/mod.rs
+++ application/src/routes/api/servers/_server_/ws/mod.rs
@@ -8,7 +8,7 @@ mod permissions;
 pub fn router(state: &State) -> OpenApiRouter<State> {
     OpenApiRouter::new()
         .nest("/deny", deny::router(state))
-        .nest("/push", broadcast::router(state))
+        .nest("/broadcast", broadcast::router(state))
         .nest("/permissions", permissions::router(state))
         .with_state(state.clone())
 }
